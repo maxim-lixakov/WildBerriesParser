78c375dbc62164f53b236f55698edb5341b87fbe
unknown <ilyas2707@yandex.ru>
2023-09-28 19:59:37 +0300

    parse and save data, tests

--- tests/test_wbparser.py
+++ tests/test_wbparser.py
@@ -1,28 +1,81 @@
-import pytest
-from unittest.mock import Mock
+import csv
+import json
+import os
+from os import listdir
+from os.path import join, isfile
 
 from wbparser.main import WBparser
 
 
-@pytest.fixture
-def mock_parser():
-    parser = WBparser('11152183')
-    parser.result = Mock(return_value={'brand': 'O`SHADE',
-                                            'colors': [{'id': 0, 'name': 'черный'}],
-                                            'diffPrice': False,
-                                            'id': '11152183',
-                                            'name': 'Ботинки женские натуральная кожа осенние',
-                                            'pics': 16,
-                                            'priceU': 757400,
-                                            'qty': 1012,
-                                            'questions': 1940,
-                                            'rating': 4.7,
-                                            'salePriceU': 560400,
-                                            'sizes': ['36', '37', '38', '39', '40', '41', '42', '43']})
-    return parser
+parser = WBparser()
+parser.parse_data(ids=[11152183, 87628789], urls=['https://www.wildberries.ru/catalog/155761175/detail.aspx'])
 
 
-def test_parse_data(mock_parser):
-    result = mock_parser.parse_data()
-    assert result is not None
-    assert 'id', 'name' in result
+mock_result = [
+    {
+        'id': '11152183',
+        'name': 'Ботинки женские демисезонные натуральная кожа на шнуровке',
+        'brand': 'O`SHADE',
+        'priceU': 7574,
+        'salePriceU': 5529,
+        'picsAmt': 16,
+        'colors': [{'name': 'черный', 'id': 0}],
+        'sizes': ['36', '37', '38', '39', '40', '41', '42', '43'],
+        'qty': 566,
+        'rating': 4.7,
+        'feedbacksAmt': 2055,
+        'supplierId': 18744
+    }
+]
+
+
+def test_parse_data_by_ids():
+
+    assert parser.result is not None
+    assert 11152183 in parser.ids
+    assert 87628789 in parser.ids
+    assert 'O`SHADE' in parser.brand
+    assert 'T.TACCARDI' in parser.brand
+
+
+def test_parse_data_by_urls():
+
+    assert parser.result is not None
+    assert '155761175' in parser.ids
+    assert 'Avanti' in parser.brand
+
+
+def test_save_data_to_json():
+
+    mock_parser = WBparser()
+    mock_parser.result = mock_result
+
+    mock_parser.save_data(file_name='testfileJSON')
+
+    assert 'testfileJSON.json' in [f for f in listdir(os.getcwd()) if isfile(join(os.getcwd(), f))]
+
+    with open('testfileJSON.json', 'r') as f:
+        data = json.loads(''.join(f.readlines()))[0]
+
+    assert data['id'] == '11152183'
+    assert data['rating'] == 4.7
+    assert data['sizes'] == ['36', '37', '38', '39', '40', '41', '42', '43']
+    assert data['colors'] == [{'name': 'черный', 'id': 0}]
+
+
+def test_save_data_to_csv():
+
+    mock_parser = WBparser()
+    mock_parser.result = mock_result
+
+    mock_parser.save_data(file_name='testfileCSV', file_format='csv')
+
+    assert 'testfileCSV.csv' in [f for f in listdir(os.getcwd()) if isfile(join(os.getcwd(), f))]
+
+    with open('testfileCSV.csv', mode='r') as f:
+        csv_reader = csv.DictReader(f)
+        data = [row for row in csv_reader][0]
+
+    assert data['id'] == '11152183'
+    assert data['rating'] == '4.7'
+    assert data['sizes'] == "['36', '37', '38', '39', '40', '41', '42', '43']"
